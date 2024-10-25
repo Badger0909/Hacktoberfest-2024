693a7491a9e09666b7a8507fc1677b8a002e80d8
Ujjwalpreet-Singh <ujjwalpreet006@gmail.com>
2024-10-25 15:46:14 +0530

    Remove case-sensitivity

--- python/rock-paper-scissors/rps.py
+++ python/rock-paper-scissors/rps.py
@@ -4,27 +4,28 @@ import random
 wins = 0
 total_games = 0
 playing = True
+outcomes = ["rock", "paper", "scissors"]
 # Playing loop
 while playing:
     print(f"You won {wins} out of {total_games} games!")
     print("Choose Rock, Paper, or Scissors:")
-    player1_input = input()
+    player1_input = input().lower()
 
     # Makes sure your input had correct capitalization
-    while player1_input not in ["Rock", "Paper", "Scissors"]:
-        print("Please try again, input is case sensitive (Rock, Paper, or Scissors):")
+    while player1_input not in outcomes:
+        print("Please try again")
         player1_input = input()
     
     # Computer's choice
-    player2_input = random.choice(["Rock", "Paper", "Scissors"])
+    player2_input = random.choice(outcomes)
     print(f"Computer picked {player2_input}")
 
     # Determine the outcome
     if player1_input == player2_input:
         print("It's a tie!")
-    elif (player1_input == "Rock" and player2_input == "Paper") or \
-         (player1_input == "Paper" and player2_input == "Scissors") or \
-         (player1_input == "Scissors" and player2_input == "Rock"):
+    elif (player1_input == "rock" and player2_input == "paper") or \
+         (player1_input == "paper" and player2_input == "scissors") or \
+         (player1_input == "scissors" and player2_input == "rock"):
         print("You lose!")
     else:
         print("You win!")
@@ -35,7 +36,7 @@ while playing:
     # Ask if the player wants to play again
     print("Would you like to play again? (Y/N)")
     answer = input()
-    if answer == "N":
+    if answer.upper() == "N":
         playing = False
 # Final message
 print("Thanks for playing!")
